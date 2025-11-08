89c0b4ed421a5f12e25c970205648b05cb3de9f8
yildizkonaklari <ordulusel@gmail.com>
2025-11-09 00:48:57 +0300

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-export function Card({ children, className = "" }) {
-  return (
-    <div className={`bg-white rounded-xl ${className}`}>{children}</div>
-  );
-}
-
-export function CardContent({ children, className = "" }) {
-  return <div className={className}>{children}</div>;
-}
